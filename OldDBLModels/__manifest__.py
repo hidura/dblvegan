85efc1e2ea447c68ae03ea0b227a2bf22648ceba
José Romero <joserm25@hotmail.com>
2024-10-29 00:25:02 -0400

    Update __manifest__.py

--- OldDBLModels/__manifest__.py
+++ OldDBLModels/__manifest__.py
@@ -2,12 +2,13 @@
 
 {
     'name': 'OldDBLVeganSer',
-    'version': '16.0.9',
+    'version': '16.0.10',
     'summary': 'DBL Vegan customizations',
     'sequence': 265,
     'category': "Stock",
     'author': 'José Romero',
     'depends': ['base', 'stock', 'account', 'point_of_sale'],
+    'license': 'LGPL-3',
     'data': [
         'views/stock_scrap.xml'
     ],
